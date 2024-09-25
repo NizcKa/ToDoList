82cbeba7dbc085ba90913a58fd2ddc91fd0e3589
ZcinKa <nicholas_riley_landicho@dlsl.edu.ph>
2024-09-25 22:07:24 +0800

    Literally held together by thoughts and prayers but it works. Will (maybe) work on this tomorrow

--- src/App.js
+++ src/App.js
@@ -14,39 +14,54 @@ function App() {
     }
   }
 
-  const completeToDo = (taskIndex) => { //will fill later
+  const completeToDo = (taskIndex) => {
+    const updatedToDos = toDos.map((toDo, index) =>
+      index === taskIndex ? { ...toDo, isComplete: !toDo.isComplete } : toDo
+    );
+    setToDos(updatedToDos);
+  };
 
-  }
+  const editToDo = (taskIndex) => {
+    const updatedToDos = toDos.map((toDo, index) =>
+      index === taskIndex ? { ...toDo, isEditing: true } : toDo
+    );
+    setToDos(updatedToDos);
+  };
 
-  const editToDo = (taskIndex) => { //will fill later
-    
-  }
+  const saveToDo = (taskIndex, newText) => {
+    const updatedToDos = toDos.map((toDo, index) =>
+      index === taskIndex ? { ...toDo, text: newText, isEditing: false } : toDo
+    );
+    setToDos(updatedToDos);
+  };
 
-  const removeToDo = (taskIndex) => {  //will fill later
-    
-  }
+  const removeToDo = (taskIndex) => {
+    setToDos(toDos.filter((_, index) => index !== taskIndex));
+  };
 
 
   return (
 
     <div className="App">
       <div>
-      <h1>To Do List</h1>
+        <h1>To Do List</h1>
 
-        {toDos.length === 0 ? ( 
+        {toDos.length === 0 ? (
           <p>No to-dos available. Add one and get started!</p>
         ) : (
           <ul>
-          {toDos.map((toDo, taskIndex) => (
-            <ToDoItem
-              key={taskIndex} 
-              toDo={toDo} 
-              completeToDo={completeToDo}
-              editToDo={editToDo}
-              removeToDo={removeToDo}
-            />
-          ))}
-        </ul>
+            {toDos.map((toDo, taskIndex) => (
+              <ToDoItem
+                key={taskIndex}
+                toDo={toDo}
+                taskIndex={taskIndex}
+                completeToDo={completeToDo}
+                editToDo={editToDo}
+                saveToDo={saveToDo}  // Pass saveToDo function
+                removeToDo={removeToDo}
+              />
+            ))}
+          </ul>
         )}
 
         
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,14 +1,38 @@
 import React, { useState } from 'react';
 
-function ToDoItem({taskIndex,toDo,completeToDo,editToDo,removeToDo }) {
+function ToDoItem({ taskIndex, toDo, completeToDo, editToDo, saveToDo, removeToDo }) {
+  const [editText, setEditText] = useState(toDo.text);
+  const [isComplete, setIsComplete] = useState(false);
+
+  const handleSave = () => {
+    saveToDo(taskIndex, editText);
+  };
 
   return (
     <li>
-      <span>{toDo.text}</span>
-      <button onClick={() => completeToDo(taskIndex)}>Complete</button>
-      <button onClick={() => editToDo(taskIndex)}>Edit</button>
-      <button onClick={() => removeToDo(taskIndex)}>Remove</button>
-  
+      {toDo.isEditing ? (
+        <>
+          <input
+            type="text"
+            value={editText}
+            onChange={(e) => setEditText(e.target.value)}
+          />
+          <button onClick={handleSave}>Save</button>
+        </>
+      ) : (
+        <>
+          <span
+            style={{ textDecoration: toDo.isComplete ? 'line-through' : 'none' }}
+          >
+            {toDo.text}
+          </span>
+          <button onClick={() => completeToDo(taskIndex)}>
+            {toDo.isComplete ? 'Undo' : 'Complete'}
+          </button>
+          <button onClick={() => editToDo(taskIndex)}>Edit</button>
+          <button onClick={() => removeToDo(taskIndex)}>Remove</button>
+        </>
+      )}
     </li>
   );
 }
